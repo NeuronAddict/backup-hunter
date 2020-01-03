98e94aa113592effa2a226053d744b40161c7f0b
neuronaddict <dev@lacourarie.org>
2020-01-03 22:56:00 +0100

    Fix user-agent in itests

--- itests/test_script.py
+++ itests/test_script.py
@@ -9,11 +9,11 @@ class TestScript(TestCase):
     def test_url(self):
         expected = ['http://localhost:8000/index-bak.php', 'http://localhost:8000/index.bak.php']
         args = SimpleNamespace(url='http://localhost:8000/index.php', template=False, verbose=False, proxy=False,
-                               cookie=False, urls_file=False)
+                               cookie=False, urls_file=False, user_agent=False)
         self.assertEqual(expected, search(args))
 
     def test_urlfile(self):
         expected = ['http://localhost:8000/index-bak.php', 'http://localhost:8000/index.bak.php']
         args = SimpleNamespace(url=False, template=False, verbose=False, proxy=False,
-                               cookie=False, urls_file='urls.txt')
+                               cookie=False, urls_file='urls.txt', user_agent=False)
         self.assertEqual(expected, search(args))
