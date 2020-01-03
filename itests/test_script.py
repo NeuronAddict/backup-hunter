90b8668e169dccbaec62ede10c8dd68e59831445
neuronaddict <dev@lacourarie.org>
2020-01-03 12:41:39 +0100

    Add .bak.{extension} file and fix itests.

--- itests/test_script.py
+++ itests/test_script.py
@@ -7,13 +7,13 @@ from backuphunter.search import search
 class TestScript(TestCase):
 
     def test_url(self):
-        expected = ['http://localhost:8000/index-bak.php', 'http://localhost:8000/index.php~']
+        expected = ['http://localhost:8000/index-bak.php', 'http://localhost:8000/index.bak.php']
         args = SimpleNamespace(url='http://localhost:8000/index.php', template=False, verbose=False, proxy=False,
                                cookie=False, urls_file=False)
         self.assertEqual(expected, search(args))
 
     def test_urlfile(self):
-        expected = ['http://localhost:8000/index-bak.php', 'http://localhost:8000/index.php~']
+        expected = ['http://localhost:8000/index-bak.php', 'http://localhost:8000/index.bak.php']
         args = SimpleNamespace(url=False, template=False, verbose=False, proxy=False,
                                cookie=False, urls_file='urls.txt')
         self.assertEqual(expected, search(args))
